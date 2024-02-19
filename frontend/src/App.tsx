d590ed4f65fcebb6710d4743251b36918f58e504
Vinicius Martins <martinsrvinicius@gmail.com>
2024-02-19 22:01:32 +0000

    Project Start, Router

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+import './App.css';
+import Router from './Router'
+
+const App = () => {
+  return(
+    <>
+  <Router />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/Navbar';
+import './styles.css';
+
+const Home = () => {
+    return(
+        <>
+        <Navbar />
+        <div>
+            My HOme
+        </div>
+        </>
+    );
+}
+
+export default Home;
